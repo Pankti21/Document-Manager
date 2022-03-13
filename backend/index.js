4f5815d4c209031cb750c185b389fa3312ad7344
Abhishek Uppe <ab577089@dal.ca>
2022-03-12 21:29:42 -0400

    added eb changes

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,3 @@
-// server/index.js
-
 const express = require("express");
 
 const PORT = process.env.PORT || 3001;
@@ -10,6 +8,15 @@ app.listen(PORT, () => {
     console.log(`Server listening on ${PORT}`);
 });
 
+app.get('/', (req, res) => {
+    res.send("Hello World");
+});
 
-const rootRoute = require("./app");
-app.use("/", rootRoute);
+// docker image rm -f cloud-crowd-backend
+// docker build -t cloud-crowd-backend .
+// docker rm -f f1abfa430df7
+// docker run -d -p 8080:3001 --name cloud-crowd-backend cloud-crowd-backend
+
+// installations
+
+// eb cli
