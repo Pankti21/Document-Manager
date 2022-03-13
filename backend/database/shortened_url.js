7025ecde467e87fc2ec62be7d612cc8e8642853a
Abhishek Uppe <ab577089@dal.ca>
2022-03-13 16:21:50 -0300

    created tables

--- backend/database/shortened_url.js
+++ backend/database/shortened_url.js
@@ -2,15 +2,17 @@ import {ddbClient, ProvisionedThroughput} from "./index.js";
 import {CreateTableCommand} from "@aws-sdk/client-dynamodb";
 
 const params = {
-    TableName: "group",
-    KeySchema: [{AttributeName: "id", KeyType: "HASH"}],
+    TableName: "shortened_url",
+    KeySchema: [
+        {AttributeName: "file_id", KeyType: "HASH"}
+    ],
     AttributeDefinitions: [
-        {AttributeName: "id", AttributeType: "N"}
+        {AttributeName: "file_id", AttributeType: "N"}
     ],
     ProvisionedThroughput: {...ProvisionedThroughput}
 };
 
-export const createGroupTable = async () => {
+export const createShortenedURLTable = async () => {
     try {
         return await ddbClient.send(new CreateTableCommand(params));
     } catch (err) {
@@ -18,4 +20,4 @@ export const createGroupTable = async () => {
     }
 }
 
-createGroupTable();
+createShortenedURLTable();
