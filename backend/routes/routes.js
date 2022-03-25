3171a52b1902a026f44ba76bc58ed97f7dc2a3e4
Rahul Tulani <rh321509@dal.ca>
2022-03-25 19:09:38 -0300

    added cerate group and get user list api, nodemon

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -2,15 +2,18 @@ import express from "express";
 import testGet from "../controllers/controller.js";
 import signUp from "../controllers/signup.js";
 import createGroup from "../controllers/createGroup.js";
-import {login, logout} from "../controllers/auth.js";
-import {validateToken} from "../middleware/auth.js";
+import getUsers from "../controllers/getUserList.js";
+("../controllers/getUserList");
+import { login } from "../controllers/auth.js";
+import { validateToken } from "../middleware/auth.js";
 
 const router = express.Router();
 
 router.get("/test", testGet);
 router.post("/signup", signUp);
-router.post("/create-group", validateToken, createGroup);
+router.post("/creategroup", createGroup);
 router.post("/login", login);
-router.post("/logout", validateToken, logout);
+//router.post("/logout", validateToken, logout);
+router.get("/getusers", getUsers);
 
 export default router;
