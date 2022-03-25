2b86b7a0a29f10310de0f9593f8c6872748475b2
Pankti Vyas <pn695271@dal.ca>
2022-03-25 19:13:11 -0300

    Route structure created.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,17 @@
+import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import FileModule from './components/FileModule/FileModule';
 
 function App() {
   return (
     <div className="App">
       Document Manager
+      <Router>
+        <Switch>
+          <Route exact path = "/files">
+            <FileModule/>
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
